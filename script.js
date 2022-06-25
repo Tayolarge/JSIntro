b73bcd04fc00edf0642248716350013154b711fd
Oluwatayo Olajide <olajideoluwatayo@gmail.com>
2022-06-25 18:34:46 +0100

    first update

---
+++ script.js
@@ -0,0 +1,9 @@
+let myName = 'Oluwatayo Olajide';
+let myHeight = '1.76 meters';
+let myCountry = 'Nigeria'
+
+console.log (myName)
+console.log (myHeight)
+console.log (myCountry)
+
+alert('Check Console for my work');
